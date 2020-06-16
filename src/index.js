26113719b149e3fc1284ad4ca761d17f4f2b986f
David B <dbravo7@yahoo.com>
2020-06-16 16:49:20 -0400

    Added state to Board class to feed user response to Square class

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import './index.css';
 
 
 class Square extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         value: null,
+    //     };
+    // }
     render() {
         return (
-            <button className="square">
+            <button 
+                className="square" 
+                onClick={() => this.props.onClick()}
+            >
                 {this.props.value}
             </button>
         );
@@ -15,8 +24,17 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
     renderSquare(i) {
-        return <Square value={i} />;
+        return (<Square value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+        />
+        );
     }
 
     render() {
